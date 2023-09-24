1cea583f719077c13fc96348e2bd4065b0b0a8b7
ttduyk13 <ttduyk13@gmail.com>
2023-09-25 01:47:50 +0700

    update docker file

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t duytt10-clouddevopsengin-project4  .
+docker build -t duytt10-clouddevopsengin-capstone  .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 duytt10-clouddevopsengin-project4
+docker run -p 8000:80 duytt10-clouddevopsengin-capstone
