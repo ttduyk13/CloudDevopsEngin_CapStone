ae59c01c5539c77be50cc0a48e934ca89586b66f
ttduyk13 <ttduyk13@gmail.com>
2023-09-28 17:03:42 +0700

    update upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,15 +2,15 @@
 
 # build a docker image
 echo "Build docker image"
-IMAGE_NAME="duytt10-clouddevopsengin-capstone:latest"
-docker build -t $IMAGE_NAME .
+IMAGE_NAME="duytt10-clouddevopsengin-capstone:${1:-lastest}"
+docker build -t "$IMAGE_NAME" .
 
 # Authenticate to your default registry
-ECR_URL="142774831706.dkr.ecr.us-east-1.amazonaws.com"
+ECR_URL="041996649308.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL
 
 export REPO_NAME="duytt10-clouddevopsengin-capstone"
-repository_info=$(aws ecr describe-repositories --repository-names $REPO_NAME --region us-east-1 2>&1
+repository_info=$(aws ecr describe-repositories --repository-names $REPO_NAME --region us-east-1 2>&1 | grep -i -w "RepositoryNotFoundException") || true
 if [[ $repository_info ]]; then
     echo "The repo $REPO_NAME does not exist"
     aws ecr create-repository \
@@ -18,10 +18,10 @@ if [[ $repository_info ]]; then
         --image-scanning-configuration scanOnPush=true \
         --region us-east-1
 else
-    echo "The repo $REPO_NAME exists"  
+    echo "The repo $REPO_NAME exists"
 fi
 
 # Push an image to Amazon ECR'
 docker images
-docker tag $IMAGE_NAME ${ECR_URL}/${REPO_NAME}
-docker push ${ECR_URL}/${REPO_NAME}
+docker tag "${IMAGE_NAME}" ${ECR_URL}/${REPO_NAME}:"${1:-lastest}"
+docker push ${ECR_URL}/${REPO_NAME}:"${1:-lastest}"
